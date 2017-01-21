8fedbf34d5d90f64e3607838937eeed826311f4d
Rehan Khan <rehankhantht@gmail.com>
2017-01-22 04:56:00 +0500

    Implemented custom full name method to user

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## Key requirement: company needs documentation that salaried emloyees did or did not get overtime each week
 
 ## Models
-- Post -> date:date rationale:text
-- User -> Devise
-- AdminUser -> STI
+- x Post -> date:date rationale:text
+- x User -> Devise
+- x AdminUser -> STI
 
 ## Features:
 - Approval Workflow
@@ -15,4 +15,7 @@
 - Needs to be documented if employee did not log overtime
 
 ##UI:
-- Bootstrap -> formatting
+- x Bootstrap -> formatting
+
+## Refactor TODOS:
+- Refactor user association integration test in post_spec
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(email: "test@test.com", password: "0300rehan", password_confirmation: "0300rehan", first_name: "John", last_name: "Snow")
+  end
   describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com", password: "0300rehan", password_confirmation: "0300rehan", first_name: "John", last_name: "Snow")
-    end
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -13,6 +13,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("SNOW, JOHN")
     end
   end
 end
