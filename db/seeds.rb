eb0e556ebfc4da8e574afe0ff184a5c82a278b45
Rehan Khan <rehankhantht@gmail.com>
2017-02-12 05:01:22 +0500

    Added Status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 @user = User.create(email: "test@test.com", password: "0300rehan", password_confirmation: "0300rehan", first_name: "John", last_name: "Snow")
-
 puts "1 user created"
+AdminUser.create(email: "admin@admin.com", password: "0300rehan", password_confirmation: "0300rehan", first_name: "Admin", last_name: "Khan")
+puts "1 Admin user created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
