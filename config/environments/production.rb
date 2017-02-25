2024de56ff83754be2b77f64cbd987d41f07f0ee
rehan-dev <rehankhantht@gmail.com>
2017-02-25 13:35:22 -0800

    Updated production asset management rules

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Change false to True for heroku
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -74,6 +75,10 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  # For heroku
+  config.serve_static_assets = true
+  config.assets.digest = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
